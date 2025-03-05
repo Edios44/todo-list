d282a7dad53475e5cd78b37d09db8aa6f5887f19
Edios44 <161240985+Edios44@users.noreply.github.com>
2025-03-05 06:24:22 +0000

    add Tasks view and change Tasks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { Item,TodoList } from "./todotemplate.js";
 import { format, compareAsc } from "date-fns";
 import "./style.css"
-import { newListForm,displayLists,lists } from "./todomanipulation.js"
+import { newListForm,displayLists,lists,displayContent,currentList } from "./todomanipulation.js"
 
 
 displayLists(lists)
 newListForm()
+displayContent(currentList)
 
--- src/todomanipulation.js
+++ src/todomanipulation.js
@@ -2,6 +2,11 @@ import { Item,TodoList } from "./todotemplate.js";
 
 const defaultList = new TodoList("defaultList","this is a default list")
 const lists = [defaultList]
+defaultList.addItem("example item","example description example description", "date", "2")
+defaultList.addItem("example item2","desc desc","date2", "4")
+
+
+let currentList = lists[0]
 
 const doms = (function () {
   const sidebar = document.querySelector("#sidebar")
@@ -12,7 +17,8 @@ const doms = (function () {
   const content = document.querySelector("#content")
   const confirm_btn = document.querySelector("#confirm-btn")
   const lists = document.querySelector("#lists")
-  return {sidebar,addlistform,bgdisable,newlist_btn,close_btn,confirm_btn,lists}
+  const todo = document.querySelector("#todolists")
+  return {sidebar,content,addlistform,bgdisable,newlist_btn,close_btn,confirm_btn,lists,todo}
 })()
 
 function newList(title,description) {
@@ -22,13 +28,21 @@ function newList(title,description) {
 
 function displayLists(lists){
   doms.lists.textContent=''
+  let i = 0
   const ul = document.createElement("ul")
   lists.forEach( list => {
     const li = document.createElement("li")
     li.textContent = list.title
+    li.id = i
     ul.appendChild(li)
+    i++
   });
   doms.lists.appendChild(ul)
+  
+  doms.lists.addEventListener("click", (e)=>{
+    currentList = lists[Number(e.target.id)]
+    displayContent(currentList)
+  })
 }
 
 function confirmBtn() {
@@ -58,4 +72,53 @@ function newListForm() {
   })
 }
 
-export {newList, newListForm,displayLists,lists}
+
+function confirmTask(e,obj,currentLi,input) {
+  if (event.key == 'Enter'){
+    obj.title = input.value
+    currentLi.textContent = input.value
+    console.log(obj.title)
+    currentLi.addEventListener("click", (e)=>{changeTask(e,obj)}, {once: true})
+  }
+
+  if (event.key == 'Escape') {
+    currentLi.textContent = obj.title
+    currentLi.addEventListener("click", (e)=>{changeTask(e,obj)}, {once: true})
+  }
+}
+
+function changeTask(e,obj) {
+  let id = e.target.id
+  const input = document.createElement("input")
+  input.setAttribute("type", "text")
+
+  const currentLi = document.querySelector(`#todolists ul [id='${id}']`)
+  input.value = currentLi.textContent
+  currentLi.textContent = ''
+  currentLi.appendChild(input)
+  input.addEventListener("keydown", (e)=>{ confirmTask(e,obj,currentLi,input) })
+}
+
+function displayContent(currentList) {
+  const list = document.createElement("ul")
+  const addItem_btn = document.createElement("button")
+  addItem_btn.textContent = "Add task"
+  doms.todo.textContent = ''
+
+
+  let i = 0
+  
+  currentList.list.forEach( obj => {
+    const li = document.createElement("li")
+    li.textContent = obj.title
+    li.id = i
+    list.appendChild(li)
+    i++
+    li.addEventListener("click", (e)=>{changeTask(e,obj)}, {once : true})
+  })
+  doms.todo.appendChild(list)
+  doms.todo.appendChild(addItem_btn)
+  doms.content.appendChild(doms.todo)
+}
+
+export {newList, newListForm,displayLists,lists,displayContent,currentList}
